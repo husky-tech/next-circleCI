288246429b38ce42ed6e3e3a4c4ffb454965ee46
oda_Koki <wakesubad@gmail.com>
2020-10-07 13:22:42 +0900

    fix

--- src/app/.eslintrc.js
+++ src/app/.eslintrc.js
@@ -1,22 +1,34 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true,
+    es6: true,
   },
-  extends: [
-    'plugin:react/recommended',
-    'airbnb',
-  ],
+  extends: 'airbnb',
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
-    ecmaVersion: 12,
+    ecmaVersion: 2018,
     sourceType: 'module',
   },
   plugins: [
     'react',
   ],
   rules: {
+    'no-unused-vars': 'off',
+    'no-console': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-filename-extension': 'off',
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    },
   },
 };
